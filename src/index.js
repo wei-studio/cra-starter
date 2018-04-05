e1f8122fc253cc7d39deb22042447e9f32d31b4e
halitipek <halitipek89@gmail.com>
2018-04-05 23:04:29 +0300

    unused tag removed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
 
 import ThemeProvider from './config/ThemeProvider'
 import store from './config/store'
